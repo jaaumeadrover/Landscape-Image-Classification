fb80586a873c05dee5e40557169291e4d67d406c
Jaume Adrover Fernández <jaumeadroverfernandez@gmail.com>
2023-11-08 17:58:54 +0100

    FeatureTransformer first attempt.

---
+++ FeatureTransformer.py
@@ -0,0 +1,19 @@
+import cv2
+import skimage.feature
+import numpy as np
+import matplotlib.pyplot as plt
+
+class FeatureTransformer:
+    def transform(self, image, method, params=None):
+        """
+        Transform the input image using the specified feature extraction method.
+
+        :param image: Input image as a NumPy array.
+        :param method: A function from skimage.feature (e.g., hog, local_binary_pattern, etc.).
+        :param params: Additional parameters specific to the chosen method.
+        :return: Transformed image.
+        """
+        if params is None:
+            return method(image)
+        else:
+            return method(image, **params)
